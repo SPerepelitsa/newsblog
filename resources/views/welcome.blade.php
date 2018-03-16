40ed65e2dc7bebda09f854473e4f19b43d9980dc
Sergey Perepelitsa <sergei.poltava@gmail.com>
2018-03-16 22:59:56 +0200

    DB settings changed + auth added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Who is on duty today?
                 </div>
 
                 <div class="links">
